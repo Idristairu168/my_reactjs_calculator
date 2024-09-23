2ec99e7fb7fc7a610534980b83f8285faee3ef21
Idris Tairu <dabdullahi41@gmail.com>
2024-09-23 12:08:01 +0100

    my reactjs calculator first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+import './App.css'; // You can style the overall App if needed
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React JS Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
